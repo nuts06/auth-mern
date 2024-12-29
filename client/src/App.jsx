a00943777a719ae687543f01700c061cd2f93dca
Natasha chandrawat <natashachandrawat@gmail.com>
2024-12-29 14:11:29 +0530

    Created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+import Profile from "./components/Profile"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-2xl underline red=500'>Home</div>
+  )
+}
+
+export default Home
